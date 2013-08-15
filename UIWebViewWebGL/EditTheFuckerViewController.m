a25e62f717c1eac40e468ff3b96fb620b996d3a7
Dave Mackintosh <dave@dotankstudios.com>
2013-08-15 23:26:19 +0100

    Trying to just get something cobbled together. No glasses on. Jesus titty fuck I'm tired.

--- UIWebViewWebGL/EditTheFuckerViewController.m
+++ UIWebViewWebGL/EditTheFuckerViewController.m
@@ -22,13 +22,28 @@
 
 - (void)viewDidLoad
 {
-    [super viewDidLoad];
+  [super viewDidLoad];
+  
+  [[NSNotificationCenter defaultCenter] addObserver:self
+                                           selector:@selector(keyboardWasShown:)
+                                               name:UIKeyboardDidShowNotification object:nil];
+  
+  [[NSNotificationCenter defaultCenter] addObserver:self
+                                           selector:@selector(keyboardWillBeHidden:)
+                                               name:UIKeyboardWillHideNotification object:nil];
+  
+  
   editedURL = [[[[NSFileManager defaultManager] URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask] lastObject] URLByAppendingPathComponent:@"WebGL.html" isDirectory:NO];
-  if ([[NSFileManager defaultManager] fileExistsAtPath:editedURL.path isDirectory:NULL])
+  if ([[NSFileManager defaultManager] fileExistsAtPath:editedURL.path isDirectory:NULL]) {
+    [editor setFont:[UIFont fontWithName:@"Courier" size:10]];
+    [editor setAutocorrectionType:UITextAutocorrectionTypeNo];
+    [editor setAutocapitalizationType:UITextAutocapitalizationTypeNone];
     editor.text = [NSString stringWithContentsOfURL:editedURL encoding:NSUTF8StringEncoding error:NULL];
-  else
-  {
+  } else {
     NSURL* defaultURL = [[NSBundle mainBundle] URLForResource:@"WebGL" withExtension:@"html"];
+    [editor setFont:[UIFont fontWithName:@"Courier" size:10]];
+    [editor setAutocorrectionType:UITextAutocorrectionTypeNo];
+    [editor setAutocapitalizationType:UITextAutocapitalizationTypeNone];
     editor.text = [NSString stringWithContentsOfURL:defaultURL encoding:NSUTF8StringEncoding error:NULL];
   }
 }
@@ -36,6 +51,16 @@
 -(void)textViewDidChange:(UITextView *)textView
 {
   [textView.text writeToURL:editedURL atomically:YES encoding:NSUTF8StringEncoding error:NULL];
+}
+
+- (void)keyboardWasShown:(NSNotification*)notification {
+  NSDictionary* info = [notification userInfo];
+  CGSize keyboardSize = [[info objectForKey:UIKeyboardFrameEndUserInfoKey] CGRectValue].size;
+  self.editor.frame = CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height - keyboardSize.height);
+}
+
+- (void)keyboardWillBeHidden:(NSNotification*)notification {
+  self.editor.frame = CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height);
 }
 
 @end
